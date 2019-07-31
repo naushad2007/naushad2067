8fc1b5e682a1659825508ac15e19e39edb560913
Amit Kumar <sakshi.mehra@naukri.com>
2019-07-31 14:48:44 +0530

    template improved, download not working, multiselect checkbox not implemented

--- src/component/Template/Template.js
+++ src/component/Template/Template.js
@@ -45,11 +45,11 @@ class Template extends Component {
                         <h1 id="name">{this.props.name}</h1>
                         <ul>
                             <li>
-                                <span className="call">E</span>
+                                <span className="call circle_white" ></span>
                                 <span id="mobile">+91 {this.props.mobile}</span>
                             </li>
                             <li>
-                                <span className="post">B</span>
+                                <span className="post circle_white"></span>
                                 <span id="email">{this.props.email}</span>
                             </li>
                         </ul>
@@ -57,14 +57,14 @@ class Template extends Component {
                     <div className="container">
                         <section className="edu-details">
                             <h2>
-                                <span className="edudetails">A</span>
+                                <span className="edudetails circle_blue label_bold"></span>
                                 Education
                             </h2>
                             <ul>
                                 <li>
                                     <div>
                                     {!this.props.docisHidden && <div >
-                                    <span className="key keys">Doctorate</span>
+                                    <span className="key keys label_bold">Doctorate</span>
                                     <span>{this.props.docBranch}, from {this.props.docCollege} in {this.props.docSpecilization}</span>
                                     <span>Passing Year: {this.props.docPassingMonth} {this.props.docPassingYear} ({this.props.docDegreeType})</span>
                                     <span>Marks : {this.props.docMarks} ({this.props.docGradingScheme})</span>
@@ -75,7 +75,7 @@ class Template extends Component {
                                 <li>
                                     <div>
                                     {!this.props.pgisHidden && <div>
-                                    <span className='key keys'>Post Graduation / Masters Degree or Equivilant</span>
+                                    <span className='key keys label_bold'>Post Graduation / Masters Degree or Equivilant</span>
                                     <span>{this.props.pgBranch}, from {this.props.pgCollege} in {this.props.pgSpecilization}</span>
                                     <span>Passing Year: {this.props.pgPassingMonth} {this.props.pgPassingYear} ({this.props.pgDegreeType})</span>
                                     <span>Marks : {this.props.pgMarks} ({this.props.pgGradingScheme})</span>
@@ -86,7 +86,7 @@ class Template extends Component {
                                 <li>
                                     <div>
                                     {!this.props.ugisHidden && <div>
-                                    <span className='key keys'>Graduation / Bachelors Degree or Equivilant</span>
+                                    <span className='key keys label_bold'>Graduation / Bachelors Degree or Equivilant</span>
                                     <span>{this.props.ugBranch}, from {this.props.ugCollege} in {this.props.ugSpecilization}</span>
                                     <span>Passing Year: {this.props.ugPassingMonth} {this.props.ugPassingYear} ({this.props.ugDegreeType})</span>
                                     <span>Marks : {this.props.ugMarks} ({this.props.ugGradingScheme})</span>
@@ -95,19 +95,19 @@ class Template extends Component {
                                     </div>
                                 </li>
                                 <li>
-                                    <span className="key">Class XII</span>
+                                    <span className="key label_bold">Class XII</span>
                                     <span id="class12"></span>
                                     <span>{this.props.board12}/{this.props.medium12} with {this.props.marks12}% in the year {this.props.year12}</span>
                                     <span>Maths Marks: {this.props.mathsMarks12} EnglishMarks: {this.props.englishMarks12}</span>
                                 </li>
                                 <li>
-                                    <span className="key">Class X</span>
+                                    <span className="key label_bold">Class X</span>
                                     <span id="class12"></span>
                                     <span>{this.props.board10}/{this.props.medium10} with {this.props.marks10}% in the year {this.props.year10}</span>
                                     <span>Maths Marks: {this.props.mathsMarks10} EnglishMarks: {this.props.englishMarks10}</span>
                                 </li>
                                 <li>
-                                    <span className="key">Certificate and Courses</span>
+                                    <span className="key label_bold">Certificate and Courses</span>
                                     <span>{this.props.certificateCourse},</span>
                                     <span>Duration: {this.props.courseTimeYear} year(s) and {this.props.courseTimeMonth} month(s)</span>
                                 </li>
@@ -116,36 +116,36 @@ class Template extends Component {
                         <section className="work-details"></section>
                         <section className="person-details">
                             <h2>
-                                <span className="pdetails">D</span>
+                                <span className="circle_blue pdetails "></span>
                                 personal details
                             </h2>
                             <ul>
                                 <li>
-                                    <span className="key">Current Location</span>
+                                    <span className="key label_bold">Current Location</span>
                                     <span id="currentLocation">{this.props.currentCity}, {this.props.currentState}</span>
                                 </li>
                                 <li>
-                                    <span className="key">HomeTown</span>
+                                    <span className="key label_bold">HomeTown</span>
                                     <span id="homeLocation">{this.props.homeCity}, {this.props.homeState}</span>
                                 </li>
                                 <li>
-                                    <span className="key">Date of birth:</span>
+                                    <span className="key label_bold">Date of birth:</span>
                                     <span id="dob">{this.props.dob}</span>
                                 </li>
                                 <li>
-                                    <span className="key">Gender</span>
+                                    <span className="key label_bold">Gender</span>
                                     <span id="gender">{this.props.gender}</span>
                                 </li>
                                 <li>
-                                    <span className="key">Looking for a </span>
+                                    <span className="key label_bold">Looking for a </span>
                                     <span id="workPreference">{this.props.workingType}</span>
                                 </li>
                                 <li>
-                                    <span className="key">Can start working </span>
+                                    <span className="key label_bold">Can start working </span>
                                     <span id="startWorking">{this.props.workingDate} {this.props.workingMonth} {this.props.workingYear}</span>
                                 </li>
                                 <li>
-                                    <span className="key">Preferred location </span>
+                                    <span className="key label_bold">Preferred location </span>
                                     <span id="preferedLocation">{this.props.workingLocation}</span>
                                 </li>
                             </ul>
--- src/component/Template/Template2.js
+++ src/component/Template/Template2.js
@@ -142,11 +142,11 @@ class Template2 extends Component {
                         <h1 id="name">{this.props.name}</h1>
                         <ul>
                             <li>
-                                <span className="call">E</span>
+                                <span className="call circle_white"></span>
                                 <span id="mobile">+91 {this.props.mobile}</span>
                             </li>
                             <li>
-                                <span className="post">B</span>
+                                <span className="post circle_white"></span>
                                 <span id="email">{this.props.email}</span>
                             </li>
                         </ul>
@@ -154,14 +154,14 @@ class Template2 extends Component {
                     <div className="container">
                         <section className="edu-details">
                             <h2>
-                                <span className="edudetails">A</span>
+                                <span className="edudetails circle_pink"></span>
                                 Education
                             </h2>
                             <ul>
                                 <li>
                                     <div>
                                     {!this.props.docisHidden && <div >
-                                    <span className="key keys">Doctorate</span>
+                                    <span className="key keys label_bold">Doctorate</span>
                                     <span>{this.props.docBranch}, from {this.props.docCollege} in {this.props.docSpecilization}</span>
                                     <span>Passing Year: {this.props.docPassingMonth} {this.props.docPassingYear} ({this.props.docDegreeType})</span>
                                     <span>Marks : {this.props.docMarks} ({this.props.docGradingScheme})</span>
@@ -172,7 +172,7 @@ class Template2 extends Component {
                                 <li>
                                     <div>
                                     {!this.props.pgisHidden && <div>
-                                    <span className='key keys'>Post Graduation / Masters Degree or Equivilant</span>
+                                    <span className='key keys label_bold'>Post Graduation / Masters Degree or Equivilant</span>
                                     <span>{this.props.pgBranch}, from {this.props.pgCollege} in {this.props.pgSpecilization}</span>
                                     <span>Passing Year: {this.props.pgPassingMonth} {this.props.pgPassingYear} ({this.props.pgDegreeType})</span>
                                     <span>Marks : {this.props.pgMarks} ({this.props.pgGradingScheme})</span>
@@ -183,7 +183,7 @@ class Template2 extends Component {
                                 <li>
                                     <div>
                                     {!this.props.ugisHidden && <div>
-                                    <span className='key keys'>Graduation / Bachelors Degree or Equivilant</span>
+                                    <span className='key keys label_bold'>Graduation / Bachelors Degree or Equivilant</span>
                                     <span>{this.props.ugBranch}, from {this.props.ugCollege} in {this.props.ugSpecilization}</span>
                                     <span>Passing Year: {this.props.ugPassingMonth} {this.props.ugPassingYear} ({this.props.ugDegreeType})</span>
                                     <span>Marks : {this.props.ugMarks} ({this.props.ugGradingScheme})</span>
@@ -192,19 +192,19 @@ class Template2 extends Component {
                                     </div>
                                 </li>
                                 <li>
-                                    <span className="key">Class XII</span>
+                                    <span className="key label_bold">Class XII</span>
                                     <span id="class12"></span>
                                     <span>{this.props.board12}/{this.props.medium12} with {this.props.marks12}% in the year {this.props.year12}</span>
                                     <span>Maths Marks: {this.props.mathsMarks12} EnglishMarks: {this.props.englishMarks12}</span>
                                 </li>
                                 <li>
-                                    <span className="key">Class X</span>
+                                    <span className="key label_bold">Class X</span>
                                     <span id="class12"></span>
                                     <span>{this.props.board10}/{this.props.medium10} with {this.props.marks10}% in the year {this.props.year10}</span>
                                     <span>Maths Marks: {this.props.mathsMarks10} EnglishMarks: {this.props.englishMarks10}</span>
                                 </li>
                                 <li>
-                                    <span className="key">Certificate and Courses</span>
+                                    <span className="key label_bold">Certificate and Courses</span>
                                     <span>{this.props.certificateCourse},</span>
                                     <span>Duration: {this.props.courseTimeYear} year(s) and {this.props.courseTimeMonth} month(s)</span>
                                 </li>
@@ -213,37 +213,37 @@ class Template2 extends Component {
                         <section className="work-details"></section>
                         <section className="person-details">
                             <h2>
-                                <span className="pdetails">D</span>
+                                <span className="pdetails circle_pink"></span>
                                 personal details
                             </h2>
                             <ul>
                                 <li>
-                                    <span className="key">Current Location</span>
+                                    <span className="key label_bold">Current Location</span>
                                     <span id="currentLocation">{this.props.currentCity}, {this.props.currentState}</span>
                                 </li>
                                 <li>
-                                    <span className="key">HomeTown</span>
+                                    <span className="key label_bold">HomeTown</span>
                                     <span id="homeLocation">{this.props.homeCity}, {this.props.homeState}</span>
                                 </li>
                                 <li>
-                                    <span className="key">Date of birth:</span>
+                                    <span className="key label_bold">Date of birth:</span>
                                     <span id="dob">{this.props.dob}</span>
                                 </li>
                                 <li>
-                                    <span className="key">Gender</span>
+                                    <span className="key label_bold">Gender</span>
                                     <span id="gender">{this.props.gender}</span>
                                 </li>
                                 <li>
-                                    <span className="key">Looking for a </span>
+                                    <span className="key label_bold">Looking for a </span>
                                     <span id="workPreference">{this.props.workingType}</span>
                                 </li>
                                 <li>
-                                    <span className="key">Can start working </span>
+                                    <span className="key label_bold">Can start working </span>
                                     {/* {this.props.joiningDateisHidden && <span id="startWorking">{this.props.workingDate}</span>} */}
                                     <span id="startWorking">{this.props.workingDate} {this.props.workingMonth} {this.props.workingYear}</span>
                                 </li>
                                 <li>
-                                    <span className="key">Preferred location </span>
+                                    <span className="key label_bold">Preferred location </span>
                                     <span id="preferedLocation">{this.props.workingLocation}</span>
                                 </li>
                             </ul>
